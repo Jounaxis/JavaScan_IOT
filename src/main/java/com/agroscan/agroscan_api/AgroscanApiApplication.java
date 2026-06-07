40be7d857ac53c6d0aae31c4b47b6ed3f21204b9
Malu <maluaquino1007@gmail.com>
2026-06-07 00:19:40 -0300

    Adicionando projeto

---
+++ src/main/java/com/agroscan/agroscan_api/AgroscanApiApplication.java
@@ -0,0 +1,13 @@
+package com.agroscan.agroscan_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroscanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroscanApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agroscan/agroscan_api/AgroscanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agroscan.agroscan_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroscanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
